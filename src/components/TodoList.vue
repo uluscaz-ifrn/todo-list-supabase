55ab0ef24e696b3a9f4cb7f4b830d067b2126dc9
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-17 13:40:11 -0300

    feat: adicionar tarefa

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,24 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import supabase from '../supabase';
+
+const addTarefa = async () => {
+  if (!novaTarefa.value) return;
+  
+  const { data, error } = await supabase.from('tarefas').insert([{ titulo: novaTarefa.value, concluida: false }]);
+  if (error) console.error(error);
+  else {
+    tarefas.value.unshift(data[0]);
+    novaTarefa.value = '';
+  }
+};
+
 </script>
 
-<template></template>
+<template>
+    <h1>Lista de Tarefas</h1>
+    <div class="add-task">
+      <input v-model="novaTarefa" placeholder="Nova tarefa..." />
+      <button @click="addTarefa">Adicionar</button>
+    </div>
+</template>
