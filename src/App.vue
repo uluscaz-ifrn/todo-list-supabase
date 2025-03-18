c80ae120f43de281ca60151b80e6b556cc5beda1
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-18 12:49:53 -0300

    feat:Redirecionar após Login/Cadastro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import Auth from './components/Auth.vue';
 import TodoList from './components/TodoList.vue'
+import supabase from './supabase';
 
 const user = ref(null);
 
 onMounted(async () => {
   const { data } = await supabase.auth.getUser();
   user.value = data.user;
+
+  supabase.auth.onAuthStateChange((event, session) => {
+  user.value = session?.user || null;
+});
 });
 </script>
 
 <template>
-<Auth v-if="!user" />
-<TodoList v-else />
+  <div v-if="user">
+    <TodoList />
+    <button @click="supabase.auth.signOut()">Sair</button>
+  </div>
+  <Auth v-else />
 </template>
 
 <style scoped>
